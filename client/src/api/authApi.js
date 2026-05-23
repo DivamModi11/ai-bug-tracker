c0e32b73e32910dd2d1897ac6a60e701fe1c83ee
Divam Modi <Divammodi777@gmail.com>
2026-05-23 20:12:46 +0530

    FIX auth API base URL

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { use } from "react";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/auth",
+    baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
 });
 
 export const loginUser = userData => API.post("/login", userData);
